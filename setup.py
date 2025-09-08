7822ae06698849469998cd5ae2f50b42f90f71a8
Piyush Yadav <piyushyadav416@gmail.com>
2025-09-08 15:32:03 +0530

    Setup and requirement added

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+Hypen_e_dot='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if Hypen_e_dot in requirements:
+            requirements.remove(Hypen_e_dot)
+
+
+setup(
+    name="Student Performance Project",
+    version='0.0.1',
+    author='Piyush',
+    author_email='piyushyadav416@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
